68f2c274f7ff35b5b1fca1d984b24bc9b48b352a
Nguyen <a0904914325@gmail.com>
2019-03-24 20:54:46 +0700

    finish feature insert

--- Xu_ly/Xu_ly_3L.js
+++ Xu_ly/Xu_ly_3L.js
@@ -1,8 +1,8 @@
-// var Dia_chi_Dich_vu = `http://localhost:1000`;
-// var Dia_chi_Media = `http://localhost:1001`;
+var Dia_chi_Dich_vu = `http://localhost:1000`;
+var Dia_chi_Media = `http://localhost:1001`;
 
-var Dia_chi_Dich_vu = `https://ban-game-dvdl.herokuapp.com`;
-var Dia_chi_Media = `https://ban-game-media.herokuapp.com`;
+// var Dia_chi_Dich_vu = `https://ban-game-dvdl.herokuapp.com`;
+// var Dia_chi_Media = `https://ban-game-media.herokuapp.com`;
 
 
 function Doc_Danh_sach_Game(The_loai_Game){
@@ -36,6 +36,49 @@ function Load_San_Pham(Danh_sach_Game, Th_Cha){
   });
 }
 
+function Ghi_Game_Moi(Game, The_loai_Game){
+  var Du_lieu = {}
+  var Xu_ly_HTTP = new XMLHttpRequest();
+  var Tham_so = `Ma_so_Xu_ly=Ghi_Game_${The_loai_Game}_Moi`;
+  var Dia_chi_Xu_ly = `${Dia_chi_Dich_vu}?${Tham_so}`;
+  Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false);
+  var Chuoi_gui = JSON.stringify(Game);
+  Xu_ly_HTTP.send(Chuoi_gui);
+  var Kq = Xu_ly_HTTP.responseText;
+  console.log(Kq);
+  if(Kq != "")
+    Du_lieu = JSON.parse(Kq);
+  return Du_lieu;
+}
+
+function Ghi_Media(Hinh) {
+    var Xu_ly_HTTP = new XMLHttpRequest();
+    var Dia_chi_Xu_ly = `${Dia_chi_Media}`;
+    Xu_ly_HTTP.open("POST", Dia_chi_Xu_ly, false);
+    var Chuoi_gui = JSON.stringify(Hinh);
+    Xu_ly_HTTP.send(Chuoi_gui);
+    var Kq = Xu_ly_HTTP.responseText;
+    return Kq;
+}
+
+function Tao_Ma_so_Moi_Theo_loai(Danh_sach_Game_Theo_Loai, The_loai_Game){
+  var Danh_sach_Ma_so = [];
+  Danh_sach_Game_Theo_Loai.forEach(game => {
+    if(game.The_loai == The_loai_Game){
+      var Thanh_phan_con = game.Ma_so.trim().split("_");
+      Danh_sach_Ma_so.push(parseInt(Thanh_phan_con[1]));
+    }
+  })
+
+  Danh_sach_Ma_so.sort((a, b) => {
+    return a - b;
+  })
+
+  var Ma_so_Sau_cung = Danh_sach_Ma_so[Danh_sach_Ma_so.length - 1];
+      Ma_so_Sau_cung += 1;
+  Th_Ma_so.value = The_loai_Game + "_" + Ma_so_Sau_cung.toString();
+}
+
 //tooltip
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
@@ -57,7 +100,7 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  // x[slideIndex-1].style.display = "block";  
 }
 
 //slideshow GAME MOI
@@ -76,6 +119,6 @@ function showDivs2(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  // x[slideIndex-1].style.display = "block";  
 }
 
